3e1980b9f15cb788b9dd2d511052284596bd9003
JediMasterLucy <lucia.hans@icloud.com>
2025-07-29 19:18:04 -0500

    idk what im doing anymore

---
+++ fight.js
@@ -0,0 +1,16 @@
+let lucy;
+let venti;
+let jean;
+let neuvi;
+
+function setup() {
+    lucy = new Sprite();
+    
+    venti = new Sprite();
+    jean = new Sprite();
+    neuvi = new Sprite();
+}
+
+function rules() {
+
+}
